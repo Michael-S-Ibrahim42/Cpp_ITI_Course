21d2d359b704e71dfea7645b3d443e8099fb5c81
Michael Ibrahim <michael.ibrahim042@gmail.com>
2022-05-22 11:42:10 +0200

    Add lab43

---
+++ 02_Phase2/s43/Car.hpp
@@ -0,0 +1,25 @@
+/* **************************************************************************************
+**       Author      :	Michael S. Ibrahim
+**       Date        :	May 21, 2022
+**       Version     :	V01
+**       SWC         :	Car
+**       Description :	
+** **************************************************************************************/
+#pragma once
+
+class Car
+{
+private:
+  float fuel;
+  float speed;
+  int passengers;
+public:
+  Car();
+  Car(float amount);
+  ~Car();
+  void FillFuel(float amount);
+  void Accelerate();
+  void Brake();
+  void AddPassengers(int count);
+  void Dashboard();
+};
