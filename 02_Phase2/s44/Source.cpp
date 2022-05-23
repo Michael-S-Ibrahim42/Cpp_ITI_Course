e88f8eb5b2ab8ac001738edc14b73ad763e1c4f6
Michael Ibrahim <michael.ibrahim042@gmail.com>
2022-05-23 12:46:50 +0200

    Add lab44

---
+++ 02_Phase2/s44/Source.cpp
@@ -0,0 +1,24 @@
+/* **************************************************************************************
+**       Author      :	Michael
+**       Date        :	May 23, 2022
+**       Version     :	V01
+**       SWC         :	Source
+**       Description :	
+** **************************************************************************************/
+/* STD Headers */
+#include <iostream>
+struct Point
+{
+  int x;
+  int y;
+};
+void DrawLine(Point start, Point end)
+{
+  std::cout<<start.x<<std::endl;
+}
+/* main Fn */
+int main()
+{
+
+  return(0);
+}
