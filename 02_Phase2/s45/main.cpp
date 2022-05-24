bbcb19622398184e5124d02131bd26451932a88d
Michael Ibrahim <michael.ibrahim042@gmail.com>
2022-05-24 13:53:42 +0200

    Add lab45

---
+++ 02_Phase2/s45/main.cpp
@@ -0,0 +1,19 @@
+/* **************************************************************************************
+**       Author      :	Michael Ibrahim
+**       Date        :	May 21, 2022
+**       Version     :	V01
+**       SWC         :	main
+**       Description :	
+** **************************************************************************************/
+/* STD Headers */
+#include <iostream>
+#include "Car.hpp"
+
+/* main Fn */
+int main(void)
+{
+  Car car;
+  // car.FillFuel(6);
+  car.Dashboard();
+  return(0);
+}
